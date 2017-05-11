e189913b7890a9760e05637cae22a9920c20f0ad
Jennifer Goodridge <jen@jengoodridge.com>
2017-05-10 21:32:06 -0700

    Fix PEP8 errors for Queue.py

--- Queue.py
+++ Queue.py
@@ -1,44 +1,44 @@
 class Item():
     def __init__(self, data, next=None):
         self.data = data
-        self.next = next 
-    
+        self.next = next
+
+
 class Queue():
     def __init__(self):
         self.head = None
-        self.tail = None 
-    
+        self.tail = None
+
     def push(self, data):
         new_item = Item(data)
-        if self.head == None:
+        if self.head is None:
             self.head = new_item
             self.tail = new_item
-        else:           
+        else:
             new_item = Item(data)
             self.tail.next = new_item
             self.tail = new_item
-          
+
     def pop(self):
-        if self.head == None:
-            return False 
+        if self.head is None:
+            return False
         else:
             new_head = self.head.next
-            self.head = new_head              
-  
+            self.head = new_head
+
     def peek(self):
-        return self.head 
-        
+        return self.head
+
     def isEmpty(self):
-        return self.head == None   
-        
+        return self.head is None
+
     def __str__(self):
         string = "["
         current = self.head
-        while current != None:
+        while current is not None:
             string += str(current.data)
             current = current.next
-            if current != None:
+            if current is not None:
                 string += ","
-        
-        return string + "]"
 
+        return string + "]"
