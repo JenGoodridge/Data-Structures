ca65207c5661d0eb696d3bf827c9cce5c910e0d3
Jennifer Goodridge <jen@jengoodridge.com>
2017-05-10 21:26:40 -0700

    Fix PEP8 warnings for Stack.py

--- Stack.py
+++ Stack.py
@@ -1,23 +1,21 @@
 class Item():
     def __init__(self, data, next=None):
         self.data = data
-        self.next = next 
-        
+        self.next = next
+
+
 class Stack():
     def __init__(self):
         self.head = None
-        
-    def pop(self): # Remove item from the top of the stack 
+
+    def pop(self):  # Remove item from the top of the stack
         self.head = self.head.next
-            
-    def push(self, data): #Push new item onto stack
+
+    def push(self, data):  # Push new item onto stack
         self.head = Item(data, self.head)
-                
-    def peek(self): 
+
+    def peek(self):
         return self.head
-        
+
     def isEmpty(self):
-        return self.head == None
-    
-    
-    
+        return not self.head
